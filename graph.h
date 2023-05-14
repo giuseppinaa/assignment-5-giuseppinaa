6a8749fa3116f8cdbd687bf79106721d12817665
giuseppinaa <117123377+giuseppinaa@users.noreply.github.com>
2023-05-13 23:53:05 -0400

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,136 +1,219 @@
+// Name:Giuseppina Pullara
+// Assignment-5
+// Professor: Ioannis Stamos#include <iostream>
+
 #ifndef STUDENT_GRAPH
 #define STUDENT_GRAPH
 
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
+#include "binary_heap.h"
+#include <utility>
 #include <list>
 #include <cfloat>
-#include <iostream>
+#include <queue>
+#include <unordered_map>
 
 using namespace std;
 
-// This file is for your graph implementation.
-// Add everything you need in between the "ifndef and "endif" statements.
-// Do not put anything outside those statements
-
-struct Node
-{
-    int number = -1;
-    double edgeWeight = -0.0;
-};
-
-class Vertex
+template <typename point>
+class Graph;
+// class representing a vertex node in the graph
+template <typename point>
+class PointVertexNode
 {
 public:
-    int number;
-    list<Node> Edge;
-    bool visited = false;
-    double dist = DBL_MAX;
+    friend class Graph<point>;
+    PointVertexNode() = default;
+    // constructor with a storeKey parameter
+    PointVertexNode(point storeKey) : storeIt(storeKey),
+                                      areaDist(DBL_MAX), previousPath(nullptr)
+    {
+        checkPoint = false;
+        startingPointVertex = false;
+    }
+    // getter function for area distance
+    double AreaDistance() const
+    {
+        double dummy = areaDist;
+        return dummy; // return distance from node to node
+    }
+
+private:
+    list<pair<PointVertexNode<point> *, double>> AdjacentNodes; // list of adjacent nodes and their associated weights
+    point storeIt;                                              // the point stored in the vertex node
+    double areaDist;                                            // area distance of the graph
+    bool checkPoint, startingPointVertex;                       // indicating if it is a checkpoint and if it is a starting vertex
+    PointVertexNode<point> *previousPath;                       // pointer to the previous path node
+};
+//class for comparing PointVertexNode objects based on their area distances
+template <typename point>
+class VerticesCheck
+{
+public:
+    bool operator()(const PointVertexNode<point> *Left, const PointVertexNode<point> *Right) const
+    {
+        // check betweenleft and right distance to trickle down
+        auto checkleft = Left->AreaDistance() > Right->AreaDistance();
+
+        return checkleft;
+    }
 };
 
+//graph class
+template <typename point>
 class Graph
 {
 public:
-    int NumberOfVertices;
-    vector<Vertex> graphing;
-
-    Graph() : NumberOfVertices(0) {}
-
-    Graph(int numberOfVertices)
+    unordered_map<point, PointVertexNode<point>> NodeMapping; // unorder_map
+    //reseting properties of a vertex node
+    void ResetNodeOfVertex(PointVertexNode<point> vert)
     {
-        NumberOfVertices = numberOfVertices;
-        graphing.resize(NumberOfVertices);
-        for (int i = 0; i < numberOfVertices; i++)
+        vert.previousPath = NULL; //reset previous path of the vertex
+        vert.checkPoint = false;
+        vert.areaDist = DBL_MAX;
+        vert.startingPointVertex = false; //reset starting point false
+    }
+    // Add a vertex to the graph
+    void vertex(point vertexA)
+    {
+        auto find = NodeMapping.find(vertexA);
+        auto end = NodeMapping.end();
+        if (find == end)
         {
-            graphing[i].number = i + 1;
+            NodeMapping[vertexA] = PointVertexNode<point>(vertexA);
         }
     }
-
-    void addEdge(int from, int to, double weight)
+    //connect two vertices in the graph with an edge and weight
+    void connect(point vA, point vB, double VertexWeight)
     {
+        auto nds = NodeMapping.find(vA);
+        auto nB = NodeMapping.find(vB);
+        auto end = NodeMapping.end();
 
-        Node newNode{to, weight};
-        graphing[from - 1].Edge.push_back(newNode);
-    }
-
-    static Graph buildGraph(string fileName)
-    {
-        ifstream readFile(fileName);
-        if (!readFile.good())
+        if (nds == end)
         {
-            cerr << "Error reading file" << endl;
-            exit(1);
+            NodeMapping[vA] = PointVertexNode<point>(vA); //connects them
         }
+        if (nB == end)
+        { //find for vertex b until end of graph map connects them
+            NodeMapping[vB] = PointVertexNode<point>(vB);
+        }
+        PointVertexNode<point> *nodeA = &NodeMapping[vA]; // getting pointer to node A
+        PointVertexNode<point> *nodeB = &NodeMapping[vB]; // getting pointer to node B
 
-        string line;
-        getline(readFile, line);
-        int num = stoi(line);
-
-        Graph newGraph(num);
-
-        string str;
-        while (getline(readFile, line))
+        //check if the connection already exists and update the weight if so
+        for (auto it = nodeA->AdjacentNodes.begin(); it != nodeA->AdjacentNodes.end(); ++it)
         {
-            stringstream ss(line);
-            ss >> str;
-            int vertex = stoi(str);
-
-            while (ss >> str)
+            if (it->first == nodeB)
             {
-                int connected = stoi(str);
 
-                ss >> str;
-                double weight = stod(str);
-
-                newGraph.addEdge(vertex, connected, weight);
+                it->second = VertexWeight;
+                return;
             }
         }
 
-        readFile.close();
-
-        return newGraph;
+        //if no connection exists, create a new one
+        pair<PointVertexNode<point> *, double> CreatingNewConnection = make_pair(nodeB, VertexWeight);
+        nodeA->AdjacentNodes.push_back(CreatingNewConnection);
     }
-
-    void showOutPut(string queryFile)
+    //checking the weight (distance) between two nodes in the graph
+    double CheckWeight(point nodeA, point nodeB)
     {
-        ifstream readFile(queryFile);
-        if (!readFile.good())
+        auto search = NodeMapping.find(nodeA);
+        auto end_at = NodeMapping.end();
+        if (search != end_at)
         {
-            cerr << "Error reading file" << endl;
-            exit(1);
-        }
-
-        string line;
-        while (getline(readFile, line))
-        {
-            stringstream ss(line);
-            ss >> line;
-            int vertex = stoi(line);
-
-            ss >> line;
-            int connected = stoi(line);
-
-            cout << vertex << " " << connected << ": ";
-            cout << out(vertex, connected) << endl;
-        }
-
-        readFile.close();
-    }
-
-    string out(int vertex, int connected)
-    {
-        for (auto &node : graphing[vertex - 1].Edge)
-        {
-            if (node.number == connected)
+            for (const auto &[neighbour, weight] : search->second.AdjacentNodes)
             {
-                string weightStr = to_string(node.edgeWeight);
-                size_t lastNonZero = weightStr.find_last_not_of('0');
-                return "connected " + weightStr.substr(0, lastNonZero + 1);
+                if (neighbour->storeIt == nodeB)
+                {
+                    return weight;
+                }
             }
         }
-        return "not_connected";
+        return DBL_MAX;
+    }
+    //checking if the graph contains a vertex
+    bool Contains(point VertexPointNode)
+    {
+        auto contained = NodeMapping.find(VertexPointNode);
+        auto checkuntilend = NodeMapping.end();
+        return contained != checkuntilend;
+    }
+    //implement Dijkstra's algorithm to find the shortest path in the graph
+    void DijkstraAlgo(point start)
+    {
+        priority_queue<PointVertexNode<point> *, vector<PointVertexNode<point> *>, VerticesCheck<point>> areaDistqueue; // important to use to go through priority queue
+
+        //reset node
+        for (auto &kvp : NodeMapping)
+        {
+            ResetNodeOfVertex(kvp.second);
+        }
+
+        PointVertexNode<point> *vert = &NodeMapping[start];
+        vert->areaDist = 0.0;
+        vert->startingPointVertex = true;
+        areaDistqueue.push(vert);
+
+        while (!areaDistqueue.empty())
+        {
+            vert = areaDistqueue.top();
+            areaDistqueue.pop();
+
+            if (vert->checkPoint)
+            {
+                continue;
+            }
+
+            vert->checkPoint = true;
+
+            for (auto &neighbor : vert->AdjacentNodes)
+            {
+                double dist = vert->areaDist + neighbor.second;
+                if (dist < neighbor.first->areaDist)
+                {
+                    neighbor.first->areaDist = dist;
+                    neighbor.first->previousPath = vert;
+                    areaDistqueue.push(neighbor.first);
+                }
+            }
+        }
+    }
+
+    //print the shortest path from a given node
+    void path(const point &node_vert)
+    {
+        double max = DBL_MAX; // max value
+        cout << node_vert << ": ";
+        PointVertexNode<point> *fillin = &NodeMapping[node_vert];
+        if (fillin->startingPointVertex)
+        { // default 0.0 if starting point
+            cout << fillin->storeIt << " cost: 0.0" << endl;
+            return;
+        }
+
+        if (fillin->areaDist == max)
+        { //print not possible if cant be connected
+            cout << "not_possible" << endl;
+        }
+        else
+        {
+            shortest_path(fillin);
+            cout << "cost: " << fillin->areaDist << endl;
+        } //print the cost of edge from vertex to vertex
+    }
+    //function to recursively print the shortest path
+    void shortest_path(PointVertexNode<point> *&node_vert)
+    {
+        if (node_vert->previousPath == nullptr)
+        {
+            cout << node_vert->storeIt << " ";
+        }
+        else
+        {
+            shortest_path(node_vert->previousPath);
+            cout << node_vert->storeIt << " ";
+        }
     }
 };
 
